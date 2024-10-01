89ceed52bdaaa3806834411098710dc45863ae36
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-09-30 21:15:21 -0300

    Add improvements to spotify service

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -4,18 +4,27 @@ class SpotifyService
   end
 
   def current_user_profile
-    response = Faraday.get("#{SPOTIFY_API_URL}/me") do |req|
-      req.headers["Authorization"] = "Bearer #{@access_token}"
-      req.headers["Content-Type"] = "application/json"
-    end
-    if response.status == 200
-      JSON.parse(response.body)
-    else
-      { error: response.status, message: "Failed to retrieve user profile" }
-    end
+    response = call "/me", :get
+    handle_response response
   end
 
   private
 
   SPOTIFY_API_URL = "https://api.spotify.com/v1"
+
+  def call(path, method, body = nil)
+    Faraday.send(method, "#{SPOTIFY_API_URL}/#{path}") do |req|
+      req.headers["Authorization"] = "Bearer #{@access_token}"
+      req.headers["Content-Type"] = "application/json"
+      req.body = body.to_json if body
+    end
+  end
+
+  def handle_response(response)
+    if response.status == 200
+      JSON.parse(response.body)
+    else
+      { error: response.status, message: JSON.parse(response.body) }
+    end
+  end
 end
